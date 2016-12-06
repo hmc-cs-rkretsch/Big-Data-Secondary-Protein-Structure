3272be94d8a627e313e5447962c8fb54b03ec359
Rachael Kretsch <rkretsch@SPRAGUEMAC-02.local>
2016-12-05 18:19:10 -0800

    struggles with script

--- Program/get_structure.sh
+++ Program/get_structure.sh
@@ -2,6 +2,10 @@
 
 read idseq
 
+#must have no [] in list!
+#$idseq2=${idseq:1:${#idseq}-1}
+
 for i in $idseq; do
-	rsync -avz rsync://rsync.cmbi.ru.nl/dssp/${i}* /DSSP/
+	echo rsync://rsync.cmbi.ru.nl/ddsp/${i}
+	rsync -avz rsync://rsync.cmbi.ru.nl/ddsp/${i+"dssp"}  DSSP/
 done
