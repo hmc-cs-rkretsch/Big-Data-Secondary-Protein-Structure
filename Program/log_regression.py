53429f899107748090cac710ac3ff0b893c46f96
Rachael Kretsch <rkretsch@SPRAGUEMAC-02.local>
2016-12-06 16:22:07 -0800

    got a 54% accuracy... time to compare and write paper

--- Program/log_regression.py
+++ Program/log_regression.py
@@ -9,6 +9,8 @@ finally perform logistic regression on our data!
 
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 def log_reg(filename, penalty='l2', reg=1.0):
     '''grabs the data file and performs a 3 group logistic
@@ -22,9 +24,37 @@ def log_reg(filename, penalty='l2', reg=1.0):
     
     data_test, data_train = data[:len(data)/4,:], data[len(data)/4:,:]
     
-    x_train = data_test[:,1:]
-    y_train = data_test[:,:1]
+    x_train = data_train[:,1:]
+    y_train = data_train[:,:1]
     x_test = data_test[:,1:]
     y_test = data_test[:,:1]
     
+    #trying out logistic regression...  
     
+    accuracy=[] 
+    for regl in np.linspace(1e-4,5,20):
+        logreg = linear_model.LogisticRegression(C=regl)
+
+        # we create an instance of Neighbours Classifier and fit the data.
+        logreg.fit(x_train, y_train)
+
+        result = logreg.predict(x_test)
+        accuracy += [(result==y_test.flatten()).sum()/len(result)]
+
+    plt.plot(np.linspace(1e-4,5,20), accuracy)
+    
+    #best reg is like  3.4 but any where in this range was fine
+    
+    accuracy=[] 
+    for regl in np.linspace(1e-4,5,20):
+        logreg = linear_model.LogisticRegression(C=regl,penalty='l1')
+
+        # we create an instance of Neighbours Classifier and fit the data.
+        logreg.fit(x_train, y_train)
+
+        result = logreg.predict(x_test)
+        accuracy += [(result==y_test.flatten()).sum()/len(result)]
+
+    plt.plot(np.linspace(1e-4,5,20), accuracy)
+   
+   #L2 performs better than L1!
